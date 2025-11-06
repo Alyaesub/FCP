87f5ee25680eb1cfde8a8a58429eeadfc07408b0
Alyaesub <dadz13104@gmail.com>
2025-11-06 03:00:42 -0800

    config back/front OK route test OK

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,10 +1,17 @@
 import express from 'express'
-
+import cors from 'cors'
 const app = express()
+app.use(cors())
+
 
 app.get('/', (req, res) => {
   res.send('Hello FC Provence ! 👋⚽')
 })
+
+app.get('/api/test', (req, res) => {
+  res.send({ message : 'API fonctionnelle'})
+})
+
 
 const PORT = 3000
 app.listen(PORT, () => {
