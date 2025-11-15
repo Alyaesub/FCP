822d3a861831ad32373175514b1a119a7b46978f
Alyaesub <dadz13104@gmail.com>
2025-11-15 18:00:40 +0100

    restructuration des HelloTest

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,16 +1,23 @@
 import express from 'express'
 import cors from 'cors'
 import testRoutes from './routes/test.route'
+import { dbPromise } from './database/connect';
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 
-// Utilisation du router
+
+// Utilisation du router :
+
 app.use('/api', testRoutes)
+
+
 
 
 const PORT = 3000
 app.listen(PORT, () => {
   console.log(`✅ Serveur lancé sur http://localhost:${PORT}`)
 })
+//verif de connection mySQL
+console.log(dbPromise);
