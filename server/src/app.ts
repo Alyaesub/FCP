bcd0d684d7c6d01dda3279a930cc2848a6dcfbbf
Alyaesub <dadz13104@gmail.com>
2026-02-13 22:18:37 +0100

    fix changement encodage

--- server/src/app.ts
+++ server/src/app.ts
@@ -20,12 +20,20 @@ app.use(cors())
 app.use(cors({
   origin: [
     'http://localhost:5173',
-    'http://localhost:8080'], // ← Ajoute 8080
+    'http://localhost:8080',
+    'https://xn--fcprovenal-v6a.fr',
+	  'https://www.xn--fcprovenal-v6a.fr',
+	  'http://xn--fcprovenal-v6a.fr',
+	  'http://www.xn--fcprovenal-v6a.fr'
+  ],
     
   credentials: true
 }));
 app.use(express.json({ limit: "50mb" }));  // ← AUGMENTE LA taille des fichier upload
-app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+app.use((req, res, next) => {
+  res.setHeader('Content-Type', 'application/json; charset=utf-8');
+  next();
+});
 
 
 // routes :
